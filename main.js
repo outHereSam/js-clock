9f9427e7ba5a54d8ffd3096e9bfa862285e868f2
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-24 07:43:21 +0000

    Creates a clock object

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+// Task 1: Time Object Basics
 const newDate = new Date();
 
 const hours = newDate.getHours();
 const minutes = newDate.getMinutes();
 const seconds = newDate.getSeconds();
 
-console.log(hours);
-console.log(minutes);
-console.log(seconds);
+// Task 2: Object-Oriented Clock
+const clockObj = {
+  hours: new Date().getHours(),
+  minutes: new Date().getMinutes,
+  seconds: new Date().getSeconds,
+};
+
+console.log(clockObj.hours);
