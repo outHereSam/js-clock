fe5e9f04e1f36cd65cab1c940a95cdb1de090d6c
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-24 07:39:37 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const newDate = new Date();
+
+const hours = newDate.getHours();
+const minutes = newDate.getMinutes();
+const seconds = newDate.getSeconds();
+
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
